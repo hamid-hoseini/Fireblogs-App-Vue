72315e8b3c39709a1f159b7121bf43e267d54041
Hamid <mail.hamid.hoseini@gmail.com>
2023-02-15 23:33:11 -0800

    Add Blogs view

---
+++ src/views/Blogs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="blog-card-wrap">
+    <div class="blog-cards container">
+      <div v-if="profileAdmin" class="toggle-edit">
+        <span>Toggle Editing Post</span>
+        <input type="checkbox" v-model="editPost" />
+      </div>
+      <BlogCard :post="post" v-for="(post, index) in blogPosts" :key="index" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogCard from "../components/BlogCard";
+export default {
+  name: "blogs",
+  components: { BlogCard },
+};
+</script>
+
+<style lang="scss" scoped></style>
