7fce25a191d96dae7eca92d2d83e08f01a3b3e5e
Hamid <mail.hamid.hoseini@gmail.com>
2023-02-20 23:39:37 -0800

    Add title to the browser tab using router Add routing for Login, Register and Forgot password component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,12 +2,12 @@
   <header>
     <nav class="container">
       <div class="branding">
-        <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
+        <router-link class="header" :to="{ name: 'Home'}">FireBlogs</router-link>
       </div>
       <div class="nav-links">
         <ul v-show="!mobile">
-          <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
-          <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
+          <router-link class="link" :to="{ name: 'Home'}">Home</router-link>
+          <router-link class="link" :to="{ name: 'Blogs'}">Blogs</router-link>
           <router-link class="link" to="#">Create Post</router-link>
           <router-link class="link" to="#">Login/Register</router-link>
         </ul>
@@ -16,8 +16,8 @@
     <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
     <transition name="mobile-nav">
       <ul class="mobile-nav" v-show="mobileNav">
-        <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
-        <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
+        <router-link class="link" :to="{ name: 'Home'}">Home</router-link>
+        <router-link class="link" :to="{ name: 'Blogs'}">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
         <router-link class="link" to="#">Login/Register</router-link>
         </ul>
