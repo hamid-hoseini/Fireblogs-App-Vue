f0ba4818c5f1a3203b2f82c996c7ba12de5e2d34
Hamid <mail.hamid.hoseini@gmail.com>
2023-02-28 22:31:28 -0800

    Add script and style to Forgot password view

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -23,3 +23,48 @@
     </div>
   </div>
 </template>
+
+<script>
+import email from "../assets/Icons/envelope-regular.svg";
+// import Modal from "../components/Modal";
+// import Loading from "../components/Loading";
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: "",
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    };
+  },
+  components: {
+    email,
+    // Modal,
+    // Loading,
+  },
+  methods: {
+    closeModal() {
+      this.modalActive = !this.modalActive;
+      this.email = "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
+</style>
