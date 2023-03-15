cb5f0f562771bfa15f64341b3afe843d6bc8e01a
Hamid <mail.hamid.hoseini@gmail.com>
2023-03-14 21:16:57 -0700

    Add profile initial and menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,6 +11,37 @@
           <router-link class="link" to="#">Create Post</router-link>
           <router-link class="link" :to="{ name: 'Login'}">Login/Register</router-link>
         </ul>
+        <div v-if="!user" class="profile" ref="profile">
+          <span>{{ this.$store.state.profileInitials }}</span>
+          <div v-show="!profileMenu" class="profile-menu">
+            <div class="info">
+              <p class="initials">{{ this.$store.state.profileInitials }}</p>
+              <div class="right">
+                <p>{{ this.$store.state.profileFirstName }} {{ this.$store.state.profileLastName }}</p>
+                <p>{{ this.$store.state.profileUsername }}</p>
+                <p>{{ this.$store.state.profileEmail }}</p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link class="option" to="#">
+                  <userIcon class="icon" />
+                  <p>Profile</p>
+                </router-link>
+              </div>
+              <div v-if="!admin" class="option">
+                <router-link class="option" to="#">
+                  <adminIcon class="icon" />
+                  <p>Admin</p>
+                </router-link>
+              </div>
+              <div @click="signOut" class="option">
+                <signOutIcon class="icon" />
+                <p>Sign Out</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
     <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
@@ -27,11 +58,16 @@
 
 <script>
 import menuIcon from '../assets/Icons/bars-regular.svg'
-
+import userIcon from "../assets/Icons/user-alt-light.svg";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
+import signOutIcon from "../assets/Icons/sign-out-alt-regular.svg";
 export default {
   name: "navigation",
   components: {
     menuIcon,
+    userIcon,
+    adminIcon,
+    signOutIcon,
   },
   data() {
     return {
--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -5,7 +5,12 @@ import "firebase/firestore";
 
 var firebaseConfig = {
   // ENTER YOUR CONFIG HERE
-
+  apiKey: "AIzaSyB3EpAq_7rCiReEL599QUSu5-dY8BlDN20",
+  authDomain: "fireblogs-1f952.firebaseapp.com",
+  projectId: "fireblogs-1f952",
+  storageBucket: "fireblogs-1f952.appspot.com",
+  messagingSenderId: "253179244361",
+  appId: "1:253179244361:web:66743c89c2139f74827498"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
